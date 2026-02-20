648278d26f703c234b55655d28110f5b8f770b88
adamnfish <adamnfish@gmail.com>
2026-02-20 21:58:14 +0000

    Remove old deploy script, this is no longer neededzggg

--- deploy.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-# This script is superseded by a GitHub Actions workflow for CI/CD.
-# Run `npm run build` to produce a production build in the dist/ directory.
-npm run build || exit
-
-echo "Build complete. Output is in the dist/ directory."
